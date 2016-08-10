3b27602b3437c2dad94d032c18886a3bacaf3842
Osayame Gaius-Obaseki <osayame@gmail.com>
2016-08-09 22:11:05 -0400

    Use npm watcher

--- config/dev.exs
+++ config/dev.exs
@@ -11,8 +11,8 @@ config :phoenix_chat, PhoenixChat.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin",
-                    cd: Path.expand("../", __DIR__)]]
+  watchers: [npm: ["start"]]
+
 
 
 # Watch static and templates for browser reloading.
